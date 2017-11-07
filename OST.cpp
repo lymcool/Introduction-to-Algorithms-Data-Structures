def7886539150fde30ae37e64e2d2a5e1efc81d7
lymcool <1326833121@qq.com>
2017-11-07 15:52:14 +0800

    Update OST.cpp

--- OST.cpp
+++ OST.cpp
@@ -1,29 +1,28 @@
 ////http://blog.csdn.net/lu597203933/article/details/43459035
-//
-//
-//#include <iostream>
-//#include "OST.h"
-//
-//using namespace std;
-//
-//void main() {
-//	int a[10] = { 5,4,6, 7,2,4, 1, 8, 5, 10 };
-//	OSRBTree osbrt(a[0]);
-//	for (int i = 1;i < 10;i++)
-//		osbrt.insertOSRBTree(osbrt.root,a[i]);
-//	cout << "ÖÐÐò±éÀúµÄ½á¹û£º"<<endl;
-//	osbrt.inorderOSRBTree(osbrt.root);
-//	cout << endl;
-//
-//	int ith = 6;
-//	BSTNode* rank = osbrt.os_select(osbrt.root, ith);
-//	if (rank)
-//		cout << "ÅÅÃû" << ith << ":" << rank->val << endl;
-//	else
-//		cout << "ÅÅÃû" << ith << "²»´æÔÚ¡£" << rank->val << endl;
-//
-//	int x = 6;
-//	cout << x << "ÅÅÃûÎª£º";
-//	cout << osbrt.os_rank(osbrt.root,x) << endl;
-//	system("pause");
-//}
+
+#include <iostream>
+#include "OST.h"
+
+using namespace std;
+
+void main() {
+	int a[10] = { 5,4,6, 7,2,4, 1, 8, 5, 10 };
+	OSRBTree osbrt(a[0]);
+	for (int i = 1;i < 10;i++)
+		osbrt.insertOSRBTree(osbrt.root,a[i]);
+	cout << "ä¸­åºéåŽ†çš„ç»“æžœï¼š"<<endl;
+	osbrt.inorderOSRBTree(osbrt.root);
+	cout << endl;
+
+	int ith = 6;
+	BSTNode* rank = osbrt.os_select(osbrt.root, ith);
+	if (rank)
+		cout << "æŽ’å" << ith << ":" << rank->val << endl;
+	else
+		cout << "æŽ’å" << ith << "ä¸å­˜åœ¨ã€‚" << rank->val << endl;
+
+	int x = 6;
+	cout << x << "æŽ’åä¸ºï¼š";
+cout << osbrt.os_rank(osbrt.root,x) << endl;
+	system("pause");
+}
