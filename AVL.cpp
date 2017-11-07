8c0e56c226cc6ef805eec112a0d044ba06e1a203
lymcool <1326833121@qq.com>
2017-11-07 11:41:56 +0800

    AVL

---
+++ AVL.cpp
@@ -0,0 +1,30 @@
+//#include "AVL.h"
+//#include<iostream>
+//
+//
+//using namespace std;
+//
+//void main() {
+//
+//	avl<int> avltree ;
+//	avltree.insert(32);
+//	avltree.insert(21);
+//	avltree.insert(46);
+//	avltree.insert(54);
+//	avltree.insert(16);
+//	avltree.insert(38);
+//	avltree.insert(70);
+//	cout << "inorder tree walk is: ";
+//
+//	avlNode<int>* root = avltree.getRoot();
+//	avltree.preOrderTraverse(root);
+//	//avltree.inorder_tree_traverse();
+//	//avltree.preOrder(root);
+//	
+//	cout << endl;
+//	cout << "root of the tree is: "<<root->key<<endl;
+//
+//
+//		system("pause");
+//		exit(0);
+//}
