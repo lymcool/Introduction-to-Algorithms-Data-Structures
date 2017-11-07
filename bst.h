357c12444a28fe77745cb36c4f8d823f9774d7ba
lymcool <1326833121@qq.com>
2017-11-07 15:58:58 +0800

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,48 +1,48 @@
-//#pragma once
+#pragma once
 ////http://www.cnblogs.com/zhoutaotao/p/4096237.html
-//
-//#include <iostream>
-//#include<stack>
-//using namespace std;
-//
-//template<class T>
-//class BinarySearchTreeNode {
-//public:
-//	T key;
-//	BinarySearchTreeNode<T>* left;
-//	BinarySearchTreeNode<T>* right;
-//	BinarySearchTreeNode<T>* parent;
-//};
-//template<class T>
-//class BinarySearchTree {
-//public:
-//	BinarySearchTree();
-//	void tree_insert(const T& elem);
-//	int tree_remove(const T& elem);
-//	BinarySearchTreeNode<T>* tree_search(const T& elem)const;
-//	BinarySearchTreeNode<T>* tree_max(BinarySearchTreeNode<T>* root)const;
-//	BinarySearchTreeNode<T>* tree_min(BinarySearchTreeNode<T>* root)const;
-//	BinarySearchTreeNode<T>* tree_preccesor(const T& elem)const;
-//	BinarySearchTreeNode<T>* tree_successor(const T& elem)const;
-//	bool empty()const;
-//	void inorder_tree_traverse() const;
-//	BinarySearchTreeNode<T>* getRoot()const { return root; };
-//	void transplant(BinarySearchTreeNode<T>* u, BinarySearchTreeNode<T>* v);
-//private:
-//	BinarySearchTreeNode<T>* root;
-//};
-//
-////¹¹Ôìº¯Êı
-//template<class T>
-//BinarySearchTree<T>::BinarySearchTree() {
-//	root = NULL;
-//}
-////²åÈë£¨´Ó¸ù½Úµã¿ªÊ¼ÕÒ£¬ºó²å£©
-//template<class T>
-//void BinarySearchTree<T>::tree_insert(const T& elem) {
-//	if (!empty()) {
-//		BinarySearchTreeNode<T>* newNode = new BinarySearchTreeNode<T>;
-//		newNode->key = elem;
+
+#include <iostream>
+#include<stack>
+using namespace std;
+
+template<class T>
+class BinarySearchTreeNode {
+public:
+	T key;
+	BinarySearchTreeNode<T>* left;
+	BinarySearchTreeNode<T>* right;
+	BinarySearchTreeNode<T>* parent;
+};
+template<class T>
+class BinarySearchTree {
+public:
+	BinarySearchTree();
+	void tree_insert(const T& elem);
+	int tree_remove(const T& elem);
+	BinarySearchTreeNode<T>* tree_search(const T& elem)const;
+	BinarySearchTreeNode<T>* tree_max(BinarySearchTreeNode<T>* root)const;
+	BinarySearchTreeNode<T>* tree_min(BinarySearchTreeNode<T>* root)const;
+	BinarySearchTreeNode<T>* tree_preccesor(const T& elem)const;
+	BinarySearchTreeNode<T>* tree_successor(const T& elem)const;
+	bool empty()const;
+	void inorder_tree_traverse() const;
+	BinarySearchTreeNode<T>* getRoot()const { return root; };
+	void transplant(BinarySearchTreeNode<T>* u, BinarySearchTreeNode<T>* v);
+private:
+	BinarySearchTreeNode<T>* root;
+};
+
+//æ„é€ å‡½æ•°
+template<class T>
+BinarySearchTree<T>::BinarySearchTree() {
+	root = NULL;
+}
+//æ’å…¥ï¼ˆä»æ ¹èŠ‚ç‚¹å¼€å§‹æ‰¾ï¼Œåæ’ï¼‰
+template<class T>
+void BinarySearchTree<T>::tree_insert(const T& elem) {
+	if (!empty()) {
+		BinarySearchTreeNode<T>* newNode = new BinarySearchTreeNode<T>;
+/		newNode->key = elem;
 //		newNode->left = NULL;
 //		newNode->right = NULL;
 //		newNode->parent = NULL;
@@ -70,10 +70,10 @@
 //	}
 //
 //}
-////É¾³ı(Ö»ÓĞ×óº¢×Ó£»Ö»ÓĞÓÒº¢×Ó£»×óÓÒº¢×Ó¶¼´æÔÚ)
+////åˆ é™¤(åªæœ‰å·¦å­©å­ï¼›åªæœ‰å³å­©å­ï¼›å·¦å³å­©å­éƒ½å­˜åœ¨)
 //template<class T>
 //int  BinarySearchTree<T>::tree_remove(const T& elem) {
-//	BinarySearchTreeNode<T>* X = tree_search(elem);//ÒªÉ¾³ıµÄ½Úµã¡¢
+//	BinarySearchTreeNode<T>* X = tree_search(elem);//è¦åˆ é™¤çš„èŠ‚ç‚¹ã€
 //	if (!X)
 //		return -1;
 //	if (X->left == NULL)
@@ -81,7 +81,7 @@
 //	else if(X->right == NULL)
 //		transplant(X, X->left);
 //	else {
-//		BinarySearchTreeNode<T>* pb = tree_min(X->right);//ÒªÉ¾³ıµÄ½ÚµãµÄºó¼Ì
+//		BinarySearchTreeNode<T>* pb = tree_min(X->right);//è¦åˆ é™¤çš„èŠ‚ç‚¹çš„åç»§
 //		if (pb->parent!= X) {
 //			transplant(pb,pb->right);
 //			pb->right=X->right;
@@ -94,7 +94,7 @@
 //	return 0;
 //}
 //
-////ËÑË÷
+////æœç´¢
 //template<class T>  
 //BinarySearchTreeNode<T>*  BinarySearchTree<T>::tree_search(const T& elem)const {
 //	BinarySearchTreeNode<T>* p = root;
@@ -108,7 +108,7 @@
 //	}
 //	return p;
 //}
-////×î´ó(²»¶ÏÑØ×ÅÓÒ×ÓÊ÷ÕÒÏÂÈ¥)
+////æœ€å¤§(ä¸æ–­æ²¿ç€å³å­æ ‘æ‰¾ä¸‹å»)
 //template<class T>
 //BinarySearchTreeNode<T>* BinarySearchTree<T>::tree_max(BinarySearchTreeNode<T>* root)const {
 //	BinarySearchTreeNode<T>* p = root;
@@ -116,7 +116,7 @@
 //		p = p->right;
 //	return p;
 //}
-////×îĞ¡(²»¶ÏÑØ×Å×ó×ÓÊ÷ÕÒÏÂÈ¥)
+////æœ€å°(ä¸æ–­æ²¿ç€å·¦å­æ ‘æ‰¾ä¸‹å»)
 //template<class T>
 //BinarySearchTreeNode<T>* BinarySearchTree<T>::tree_min(BinarySearchTreeNode<T>* root) const {
 //	BinarySearchTreeNode<T>* p = root;
@@ -125,7 +125,7 @@
 //	return p;
 //}
 //
-////Ç°¼Ì
+////å‰ç»§
 //template<class T>
 //BinarySearchTreeNode<T>* BinarySearchTree<T>::tree_preccesor(const T& elem)const {
 //	BinarySearchTreeNode<T>* x = tree_search(elem);
@@ -145,7 +145,7 @@
 //		return p;
 //    }
 //}
-////ºó¼Ì
+////åç»§
 //template<class T>
 //BinarySearchTreeNode<T>* BinarySearchTree<T>::tree_successor(const T& elem)const {
 //	BinarySearchTreeNode<T>* x = tree_search(elem);
@@ -166,7 +166,7 @@
 //	}
 //}
 //
-////Ìæ»»(ÓÃvÌæ»»u)
+////æ›¿æ¢(ç”¨væ›¿æ¢u)
 //template <class T>
 //void BinarySearchTree<T>::transplant(BinarySearchTreeNode<T>* u, BinarySearchTreeNode<T>* v) {
 //	if (u->parent == NULL)
@@ -179,14 +179,14 @@
 //		v->parent = u->parent;
 //}
 //
-////Çå¿Õ
+////æ¸…ç©º
 //template <class T>
 //bool BinarySearchTree<T>::empty() const
 // {
 //     return (NULL == root);
 // }
 //
-//////ÖĞĞò±éÀú£¨½èÖúÕ»£©
+//////ä¸­åºéå†ï¼ˆå€ŸåŠ©æ ˆï¼‰
 //template <class T>
 //void BinarySearchTree<T>::inorder_tree_traverse()const {
 //	if (root!=NULL) {
